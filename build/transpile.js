6e874b82c4ad7f7aab2bac9101b4029e8499a12e
Zoltán Nagy <abesto@abesto.net>
2022-12-21 11:32:50 +0000

    fix: `build/connfig.js` made `syncStatic` delete all `.js` files in `dist`

--- build/transpile.js
+++ build/transpile.js
@@ -1,6 +1,7 @@
 const esbuild = require("esbuild");
 const chokidar = require("chokidar");
 const path = require("path");
+const fg = require("fast-glob");
 
 const { src, dist } = require("./config");
 
@@ -8,14 +9,18 @@ async function rebuild(file) {
   const relative = path.relative(src, file);
   const distFile = path.resolve(dist, relative).replace(/\.ts$/, ".js");
   console.log(`Rebuilding ${relative}...`);
-  await esbuild.build({
-    entryPoints: [file],
-    bundle: true,
-    minify: false,
-    sourcemap: "inline",
-    outfile: distFile,
-    format: "esm",
-  });
+  try {
+    await esbuild.build({
+      entryPoints: [file],
+      bundle: true,
+      minify: false,
+      sourcemap: "inline",
+      outfile: distFile,
+      format: "esm",
+    });
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 chokidar
