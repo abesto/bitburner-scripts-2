269fb0d168d6e1ed1681c88192d36fb1b2dc30d1
Zoltán Nagy <abesto@abesto.net>
2022-12-29 19:15:50 +0000

    Use `esbuild` watch instead of `chokidar` watch; this triggers rebuild on imported code change

--- build/transpile.js
+++ build/transpile.js
@@ -1,7 +1,6 @@
 const esbuild = require("esbuild");
 const chokidar = require("chokidar");
 const path = require("path");
-const fg = require("fast-glob");
 
 const { src, dist } = require("./config");
 
@@ -10,20 +9,29 @@ async function rebuild(file) {
   const distFile = path.resolve(dist, relative).replace(/\.ts$/, ".js");
   console.log(`Rebuilding ${relative}...`);
   try {
-    await esbuild.build({
-      entryPoints: [file],
-      bundle: true,
-      minify: false,
-      sourcemap: "inline",
-      outfile: distFile,
-      format: "esm",
-    });
+    await esbuild
+      .build({
+        entryPoints: [file],
+        bundle: true,
+        minify: false,
+        sourcemap: "inline",
+        outfile: distFile,
+        format: "esm",
+        watch: {
+          onRebuild(error) {
+            if (error) console.error("watch build failed:", error);
+            else console.log("watch build succeeded: " + file);
+          },
+        },
+        watch: true,
+        incremental: true,
+      })
+      .then(() => {
+        console.log("Started watching " + file);
+      });
   } catch (e) {
     console.error(e);
   }
 }
 
-chokidar
-  .watch(`${src}/bin/**/*.ts`, { persistent: true })
-  .on("add", rebuild)
-  .on("change", rebuild);
+chokidar.watch(`${src}/bin/**/*.ts`, { persistent: true }).on("add", rebuild);
