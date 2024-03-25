5041c0c9cc2d375c43db3d761a1455ea36dc71fa
Zoltán Nagy <abesto@abesto.net>
2024-03-25 22:42:20 +0000

    Fix: `aliases` was broken because `ls` omits starting `/`

--- src/bin/aliases.ts
+++ src/bin/aliases.ts
@@ -1,17 +1,17 @@
-import { NS } from '@ns';
+import { NS } from "@ns";
 
 export async function main(ns: NS): Promise<void> {
   const hostname = ns.getHostname();
   ns.tprint(
     "Copy-paste:\n" +
       ns
-        .ls(hostname)
-        .filter((file) => file.match(/^\/bin\/[^/]*\.js$/))
+        .ls(hostname, "/bin/")
+        .filter((file) => file.endsWith(".js") && file.split("/").length === 2)
         .map(
           (file) =>
             `alias ${file
               .replace(".js", "")
-              .replace("/bin/", "")}="run ${file}";`
+              .replace(/^bin\//, "")}="run ${file}";`
         )
         .join("\n")
   );
